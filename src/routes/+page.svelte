4481eabe590bf3760078ef4bc403c482b393ff99
Jackie Luc <jackieluc8487@gmail.com>
2025-01-06 12:44:52 -0800

    added tailwind and added style to header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+
+
+<h1 class="text-9xl text-center bg-gradient-to-r to-red-700 from-orange-500 via-purple-700 underline border-dashed 
+rounded transition-all duration-500 hover:rounded-full
+ border-teal-400 border-8 decoration-blue-300 text-lime-300">Welcome to our website</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
