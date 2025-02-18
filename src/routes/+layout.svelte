eb325d72f5483a47c38a9d9271214665f75132a3
Jackie Luc <jackieluc8487@gmail.com>
2025-02-17 17:11:32 -0800

    Revert "removed dark mode tags from unordered list, changed hover to match color of title and Home"
    This reverts commit b4a4944044272eb2090dafaf86432c415654c36f.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -32,21 +32,21 @@ Accent Colors (For Links & Buttons)
         </svg>
     </button>
     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border-2 border-warm-mocha-brown rounded-lg bg-antique-rose md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-antique-rose">
+      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border-2 border-warm-mocha-brown rounded-lg bg-blush-pink md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-antique-rose">
         <li>
           <a href="#" class="block py-2 px-3 text-deep-charcoal bg-ivory-cream rounded-sm md:bg-transparent md:text-dark-sage md:p-0 " aria-current="page">Home</a>
         </li>
         <li>
-          <a href="#" class="block py-2 px-3 text-white rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-dark-sage md:p-0">About</a>
+          <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-sage-green md:p-0 dark:text-white">About</a>
         </li>
         <li>
-          <a href="#" class="block py-2 px-3 text-white rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-dark-sage md:p-0 ">Services</a>
+          <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
         </li>
         <li>
-          <a href="#" class="block py-2 px-3 text-white rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-dark-sage md:p-0 ">Pricing</a>
+          <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
         </li>
         <li>
-          <a href="#" class="block py-2 px-3 text-white rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-dark-sage md:p-0 ">Contact</a>
+          <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
         </li>
       </ul>
     </div>
